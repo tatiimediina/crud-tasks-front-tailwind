dd574034520c5f1873979d1bd36265f8e596fe72
Tatiana Medina <tatymedina284@gmail.com>
2024-08-28 16:37:18 -0300

    Se empezo la migracion del crud desde el front con tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html", "./src/js/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "selector",
+};
